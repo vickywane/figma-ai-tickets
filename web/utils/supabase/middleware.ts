7d30639de222a76c6a1f1a23201556537788af34
Victory Nwani <vickywane@gmail.com>
2025-07-29 12:00:16 +0100

    feat: add Hono function router, invoke supabase functions via client SDK & get user details from auth headers

--- web/utils/supabase/middleware.ts
+++ web/utils/supabase/middleware.ts
@@ -1,15 +1,10 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
-import { cookies } from "next/headers";
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
-
-  const access_token = request.nextUrl.searchParams.get("access_token");
-  const refresh_token = request.nextUrl.searchParams.get("refresh_token");
-  // const cookieStore = await cookies()
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -20,7 +15,7 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
+          cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value)
           );
           supabaseResponse = NextResponse.next({
@@ -34,25 +29,16 @@ export async function updateSession(request: NextRequest) {
     }
   );
 
-  // console.log(
-  //   "COOKIE ->", cookieStore.get("sb-paxlhoeizbkwcsuqlcun-auth-token")
-  // )
-
   const tokens = request.nextUrl
     .clone()
     .pathname.split("/")
     .filter((val) => val);
 
-  // console.log("URL", tokens);
-
-  // if (access_token && refresh_token) {
   if (tokens && tokens[0] && tokens[1]) {
-    const session = await supabase.auth.setSession({
+    await supabase.auth.setSession({
       refresh_token: tokens[1],
       access_token: tokens[0],
     });
-
-    console.log("Session set:", session);
 
     // TODO: clean up the URL after setting the session & remove the tokens
     // const url = request.nextUrl.clone()
--- web/utils/supabase/server.ts
+++ web/utils/supabase/server.ts
@@ -3,8 +3,7 @@ import { cookies } from 'next/headers'
 
 export async function createSupabaseServerClient() {
   const cookieStore = await cookies()
-  // Create a server's supabase client with newly configured cookie,
-  // which could be used to maintain user's session
+ 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
