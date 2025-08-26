02fb77bb604099cdc461a24f3b515e87918a8a90
Nwani Victory <vickywane@gmail.com>
2025-08-26 15:29:12 +0100

    update: configure deployments for netlify

--- portal/vite.config.ts
+++ portal/vite.config.ts
@@ -2,14 +2,14 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlify from "@netlify/vite-plugin";
+import netlifyPlugin from '@netlify/vite-plugin-react-router'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
-    netlify(),
-    // netlifyPlugin(),
+    // netlify(),
+    netlifyPlugin(),
   ],
 });
